fae174309c227ac3e034a65acd06444894381436
JarrettMitchener <j.mitchener4@gmail.com>
2026-08-08 20:14:19 -0500

    generic shit has been done

--- BlazorBruh/Models/ISequence.cs
+++ BlazorBruh/Models/ISequence.cs
@@ -1,14 +1,24 @@
 namespace BlazorBruh.Models;
 
-public class ISequence
+public interface ISequence
 {
     public int SequenceNumber { get; set; }
 }
 
 public class FlowModel : ISequence
 {
+    public int SequenceNumber { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public int Age { get; set; }
 }
+
+public class Event : ISequence
+{
+    public int SequenceNumber { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime Time { get; set; }
+    public decimal Price { get; set; }
+}
+
